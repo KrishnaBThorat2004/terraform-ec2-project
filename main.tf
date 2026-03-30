6cea0cdb201cd21a5f5d20fe5a3774a322c93e55
Rushi <krishnathorat57@gmail.com>
2026-03-30 12:02:23 +0530

    Terraform EC2 setup

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
-provider "aws" {
-  region = "ap-south-1"
+# 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "aws_instance" "myserver" {
-  ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
+provider "aws" {
+  region  = "ap-south-1"
+
+}
+
+resource "aws_instance" "demo1" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.micro"   # ✅ FREE TIER
+  key_name      = "my_new"
 
   tags = {
-    Name = "EC2-From-Terraform"
+    Name = "Terraform-EC2"
   }
 }
